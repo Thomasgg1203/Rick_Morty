e6e45a2d3f743ce744e4b665c20b774cafe933e0
Thomasgg1203 <thomasgg1203@gmail.com>
2023-10-07 16:48:33 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import imgRickMorty from './img/rick-morty.png';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+      const api = await fetch('https://rickandmortyapi.com/api/character');
+      const characterApi = await api.json();
+      setCharacters(characterApi.results);
+  }
+  console.log(characters)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>Rick & Morty</h1>
+        {/* Verificar que la api esten llegando los datos como son */}
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+          <img src={imgRickMorty} alt='RickAndMorty' className='img-home'></img>
+          <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+          </>
+        )}
       </header>
     </div>
   );
---
+++ src/components/Characters.js
@@ -0,0 +1,50 @@
+export default function Characters(props) {
+
+  
+    //destruccion de caracteres(dividirlos)
+    const { characters, setCharacters } = props;
+
+    const resetCharacters = () => {
+        setCharacters(null);
+      };
+  return (
+    <div className="characters">
+        <h1>personajes</h1>
+        <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
+        <div className="container-characters">
+            { characters.map((character, index) => (//con parentesis es un return implicito
+                <div className="character-container" key={index}>
+                    <div>
+                        <img src={ character.image } alt={character.name}></img>
+                    </div>
+                    <div>
+                        <h3> { character.name } </h3>
+                        <h6>
+                            { character.status === "Alive" ? (
+                                <>
+                                <span className="alive">Alive</span>
+                                </>
+                            ) : (
+                                <>
+                                <span className="dead">Dead</span>
+                                </>
+                            )}
+                        </h6>
+                        <p>
+                            <span className="text-grey">Episodios: </span>
+                            <span>{character.episode.length}</span>
+                        </p>
+                        <p>
+                            <span className="text-grey">Especie: </span>
+                            <span>{character.species}</span>
+                        </p>
+                    </div>
+                </div>
+            )) }
+        </div>
+        <span className="back-home" onClick={resetCharacters}>
+            Volver a la home
+        </span>
+    </div>
+  )
+}
